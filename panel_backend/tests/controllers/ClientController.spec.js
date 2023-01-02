c3a11c7f8387df3c6dcb16d54ceeea63657abedc
Conrado Jardim <conradocjo@gmail.com>
2023-01-02 19:13:17 -0300

    Evolução do fonte

--- panel_backend/tests/controllers/ClientController.spec.js
+++ panel_backend/tests/controllers/ClientController.spec.js
@@ -3,8 +3,13 @@ var example = require('../../controllers/ClientController')
 describe('Teste da camada de controller', () => {
     it('Deve testar o método call client', () => {
         module.exports = (app) => {
-            var teste = app.controllers.ClientController.callClient(app, req)
-            console.log(teste)
+            app.controllers.ClientController.callClient(app, req)
+        }
+    })
+
+    it('Deve testar o método call client', () => {
+        module.exports = (app) => {
+            app.controllers.ClientController.callClient(null, null)
         }
     })
 })
